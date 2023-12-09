6f3f864ff7f4758960b04ecb607c70757bd42541
Guardofthestorms <xacharle@student.42.fr>
2023-12-09 19:39:41 +0100

    init ccmd à tester et a rendre moins monstrueux

--- Source/minishell.c
+++ Source/minishell.c
@@ -82,29 +82,6 @@ int	clean_cmd(t_cmd *cmd)
 	return (1);
 }
 
-int	ca_parse(t_data *d, char *input, char **env)
-{
-	d->cmd = malloc(sizeof(t_cmd));
-	if (!d->cmd)
-		return (printf("Failed malloc for t_cmd\n"));
-	d->cmd->cmds = ms_split(input, "&|");
-	if (!d->cmd->cmds)
-		return (clean_cmd(d->cmd));
-	if (sep_count(input) >= 1)
-	{
-		d->cmd->sep = rev_ms_split(input, "&|");
-		if (!(d->cmd->sep))
-			return (clean_cmd(d->cmd));
-	}
-	else
-		d->cmd->sep = NULL;
-	if (pathfinder(d->cmd, env))
-	{
-		return (clean_cmd(d->cmd));
-	}
-	return (0);
-}
-
 int	init_ccmd(t_data *d, t_ccmd *cmd)
 {
 	int		i;
@@ -121,7 +98,7 @@ int	init_ccmd(t_data *d, t_ccmd *cmd)
 			return (1) //implement free error
 		if (strs[0] && !ft_strchr(strs[0], "<") && ft_strchr(strs[0], ">"))
 		{
-			cmd->cmd = ft_strdup(strs[j]);
+			cmd->cmd = ft_strdup(strs[0]);
 			if (!cmd->cmd)
 				return (1); //implement 
 			j = 0;
@@ -130,12 +107,163 @@ int	init_ccmd(t_data *d, t_ccmd *cmd)
 			if (!cmd_args)
 				return (1); //implement err
 			cmd->cmd_arg = ms_split(cmd_args, " ");
+			if (!cmd->cmd_arg)
+				return (1); //err
+		}
+		else if (strs[0])
+		{
+			
 		}
 	}
 }
 
+int	init_ccmd(t_data *d, t_ccmd *ccmd)
+{
+	int	i;
+	int	j;
+	char	*cmd_args;
+	char	*in;
+	char	*out;
+	char	**strs;
+
+	i = 0;
+	cmd_args = NULL;
+	while (d->cmds[i])
+	{
+		strs = md_split(d->cmds[i], "\t ");
+		if (!strs)
+			return (1);
+		j = 0;
+		in = NULL;
+		out = NULL;
+		while (strs[j])
+		{
+			if (!ft_strchr(strs[j], "<") && !ft_strchr(strs[j], ">"))
+			{
+				ccmd[i]->cmd = ft_strdup(strs[j]);
+				if (!ccmd[i]->cmd)
+					return (1); //implement
+				while (strs[j] && !ft_strchr(strs[j], "<") && !ft_strchr(strs[j], ">"))
+					cmd_args = join_w_space(cmd_args, strs[j++]);
+				if (!cmd_args)
+					return (1);
+				ccmd[i]->cmd_arg = ms_split(cmd_args, " ");
+				if (!ccmd[i]->cmd_arg)
+					return (1);
+				free(cmd_args);
+			}
+			else
+			{
+				while (!ft_strncmp(strs[j], ">", ft_strlen(strs[j])) || !ft_strncmp(strs[j], ">>", ft_strlen(strs[j])))
+				{
+					j++;
+					if (!strs[j])
+						return (1); //parse error
+					out = join_w_space(out, strs[j - 1]);
+					if (!out)
+						return (1); //malloc error
+					out = join_w_tab(out, strs[j]);
+					if (!out)
+						return (1); //malloc error
+					j++;
+				}
+				while (!ft_strncmp(strs[j], "<", ft_strlen(strs[j])))
+				{
+					j++;
+					if (!strs[j])
+						return (1); //parse error
+					in = join_w_space(in, strs[j - 1]);
+					if (!in)
+						return (1); //malloc error
+					in = join_w_tab(in, strs[j]);
+					if (!in)
+						return (1); //malloc error
+					j++;
+				}
+			}
+		}
+		if (in)
+		{
+			ccmd[i]->in = ms_split(in, "\t");
+			if (!ccmd[i]->in)
+				return (1); //malloc error
+			free(in);
+		}
+		if (out)
+		{
+			ccmd[i]->out = ms_split(out, "\t");
+			if (!ccmd[i]->out)
+				return (1); //malloc error
+			free(out);
+		}
+		clean_strs(0, 0, 0, strs);
+		i++;
+	}
+}
+
+int	set_op_pipe(int j, t_ccmd ccmd, char *input)
+{
+	if (!ft_strncmp(input + j, "|", 1))
+	{
+		ccmd->next_op = ft_strdup("|");
+		if (!ccmd->next_op)
+			return (1); //malloc error
+	}
+	else if (!ft_strncmp(input + j, "||", 2))
+	{
+		ccmd->next_op = ft_strdup("||");
+		if (!ccmd->next_op)
+			return (1); //malloc error
+	}
+	return (0);
+}
+
+int	set_op_and(int j, t_ccmd ccmd, char *input)
+{
+	if (!ft_strncmp(input + j, "&", 1))
+	{
+		ccmd->next_op = ft_strdup("&");
+		if (!ccmd->next_op)
+			return (1); //malloc error
+	}
+	else if (!ft_strncmp(input + j, "&&", 1))
+	{
+		ccmd->next_op = ft_strdup("&&");
+		if (!ccmd->next_op)
+			return (1); //malloc error
+	}
+	return (0);
+}
+
+int	set_next_op(t_data *d, char *input)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (d->cmds[i + 1])
+	{
+		j = ft_strlen(d->cdms[i]);
+		while (input[j] == ' ' || input[j] == '\t')
+			j++;
+		if (set_op_pipe(j, d->cmd[i], input))
+			return(1); //maloc error
+		if (set_op_and(j, d->cmd[i], input))
+			return(1); //maloc error
+		i++;
+	}
+	return (0);
+}
+
 int	ca_parse(t_data *d, char *input)
 {
+	int	i;
+
+	i = 0;
+	while (input[i] == ' ' || input[i] == '\t')
+		i++;
+	if (ft_strchr(input[i], "&") || ft_strchr(input[i], "|"))
+		return (1); //parse error 1st char == & ou && ou | ou ||
 	d->cmd_count = cmd_count(input);
 	d->sep_count = sep_count(input);
 	d->seps = rev_ms_split(input, "&|");
@@ -149,6 +277,8 @@ int	ca_parse(t_data *d, char *input)
 	d->cmd = malloc(sizeof(t_ccmd) * d->cmd_count);
 	if (!d->cmd)
 		return (1); //free a implement
+	if (set_next_op(d, input))
+		return (1); //malloc error
 	if (init_ccmd(d, d->cmd))
 		return (1); //free a iplement
 }
